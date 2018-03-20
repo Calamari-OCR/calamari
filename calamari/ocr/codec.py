a6cb28f37f56a49c916b225f7a324fa328790538
Christoph Wick <wick.chr.info@gmail.com>
2018-03-20 16:37:10 +0100

    setup basic setup.py skript

--- calamari/ocr/codec.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='calamari',
-    version='0.1dev',
-    packages=['calamari', ],
-    license='GPL-v3.0',
-    long_description="OCR Engine",
-)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='calamari',
+    version='0.1dev',
+    packages=['calamari', ],
+    license='GPL-v3.0',
+    long_description="OCR Engine",
+)
