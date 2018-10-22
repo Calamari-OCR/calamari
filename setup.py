99d2052694737f0d0d49e08b9ecab52d2a753882
Christoph Wick <wick.chr.info@gmail.com>
2018-10-22 12:49:00 +0200

    Version update to 0.2.1
    Fixes #30
    Closes #31

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='calamari_ocr',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(),
     license='GPL-v3.0',
     long_description=open("README.md").read(),
