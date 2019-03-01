3b8239b71f92444399d72acc4974070d914b36cd
Christoph Wick <wick.chr.info@gmail.com>
2019-03-01 14:37:20 +0100

    Fixed #70: Float data type
    running statistics forcing float as data type

--- calamari_ocr/utils/running_statistics.py
+++ calamari_ocr/utils/running_statistics.py
@@ -1,10 +1,8 @@
 class RunningStatistics:
-    def __init__(self, max_size, initial_values=[]):
+    def __init__(self, max_size, initial_values=None):
         self.max_size = max_size
-        self.values = initial_values
-        self.cur_sum = 0
-        if len(initial_values) > 0:
-            self.cur_sum = sum(self.values)
+        self.values = list(map(float, initial_values)) if initial_values is not None else []
+        self.cur_sum = sum(self.values)
 
         if max_size < 1:
             raise Exception("A size > 0 is required. Got {}".format(max_size))
@@ -16,6 +14,7 @@ class RunningStatistics:
         return self.cur_sum / len(self.values)
 
     def push(self, v):
+        v = float(v)
         self.values.append(v)
 
         if len(self.values) > self.max_size:
