6fd9b2f7ce62e798f3e662a6f3bd9c8c281faf62
Christoph Wick <wick.chr.info@gmail.com>
2019-06-26 14:57:31 +0200

    Fixed ExitStackWithPop for newer python versions

--- calamari_ocr/utils/contextmanager.py
+++ calamari_ocr/utils/contextmanager.py
@@ -1,6 +1,7 @@
 from contextlib import ExitStack
 from collections import deque
 
+
 class ExitStackWithPop(ExitStack):
     def pop(self, cm):
         callbacks = self._exit_callbacks
@@ -8,7 +9,18 @@ class ExitStackWithPop(ExitStack):
         found = None
         while callbacks:
             cb = callbacks.popleft()
-            if cb.__self__ == cm:
+            # Newer versions return a (bool, cb) tuple
+            if isinstance(cb, tuple):
+                # search for the cb (should be the last element)
+                for c in cb:
+                    try:
+                        if c.__self__ == cm:
+                            found = c
+                            break
+                    except AttributeError:
+                        continue
+            # Older version return the cb directly
+            elif cb.__self__ == cm:
                 found = cb
             else:
                 self._exit_callbacks.append(cb)
