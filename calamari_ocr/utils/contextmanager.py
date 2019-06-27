Calamari-OCR/calamari

commit 6fd9b2f7ce62e798f3e662a6f3bd9c8c281faf62

Fixed ExitStackWithPop for newer python versions

calamari_ocr/utils/contextmanager.py
@@ -1,6 +1,7 @@
 from contextlib import ExitStack
 from collections import deque
 
+
 class ExitStackWithPop(ExitStack):
     def pop(self, cm):
         callbacks = self._exit_callbacks
@@ -8,7 +9,18 @@ class ExitStackWithPop(ExitStack):
         found = None
         while callbacks:
             cb = callbacks.popleft()
-            if cb.__self__ == cm:
+            # Newer versions return a (bool, cb) tuple
+            if isinstance(cb, tuple):
+                # search for the cb (should be the last element)
+                for c in cb:
+                    try:
+                        if c.__self__ == cm:
+                            found = c
+                            break
+                    except AttributeError:
+                        continue
+            # Older version return the cb directly
+            elif cb.__self__ == cm:
                 found = cb
             else:
                 self._exit_callbacks.append(cb)

commit 4485bdd6a6a2b3d41e44b56547fd57565b7e08a3

Fix contextmanager for newer Python versions
The commit 6fd9b2f introduced a logic error that throws away most of the
existing stack if running on newer Python/contextlib versions. Fix this
by handling the different internal ExitStack organisation in a helper
function and reverting to the previous logic.

Fixes #116.

calamari_ocr/utils/contextmanager.py
@@ -7,23 +7,20 @@ class ExitStackWithPop(ExitStack):
         callbacks = self._exit_callbacks
         self._exit_callbacks = deque()
         found = None
+
+        def unpack_cb(cb):
+            if isinstance(cb, tuple):
+                return cb[1]
+            else:
+                return cb
+
         while callbacks:
             cb = callbacks.popleft()
-            # Newer versions return a (bool, cb) tuple
-            if isinstance(cb, tuple):
-                # search for the cb (should be the last element)
-                for c in cb:
-                    try:
-                        if c.__self__ == cm:
-                            found = c
-                            break
-                    except AttributeError:
-                        continue
-            # Older version return the cb directly
-            elif cb.__self__ == cm:
+            if unpack_cb(cb).__self__ == cm:
                 found = cb
             else:
                 self._exit_callbacks.append(cb)
         if not found:
             raise KeyError("context manager not found")
+        found = unpack_cb(found)
         found(None, None, None)