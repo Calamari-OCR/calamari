349fc26aec42d9d20d0802a55b9951cb025adcfc
s341441 <s341441@winc154.informatik.uni-wuerzburg.de>
2019-07-09 11:41:05 +0200

    changed image and gt loader

--- calamari_ocr/ocr/datasets/file_dataset.py
+++ calamari_ocr/ocr/datasets/file_dataset.py
@@ -80,12 +80,12 @@ class FileDataSet(DataSet):
         images = [] if images is None else images
         texts = [] if texts is None else texts
 
-        if mode is not DataSetMode.PRED_AND_EVAL: 
-            if mode == DataSetMode.PREDICT:
-                texts = [None] * len(images)
-    
-            if mode == DataSetMode.EVAL:
-                images = [None] * len(texts)
+        
+        if mode == DataSetMode.PREDICT or mode == DataSetMode.PRED_AND_EVAL:
+            texts = [None] * len(images)
+
+        if mode == DataSetMode.EVAL or mode == DataSetMode.PRED_AND_EVAL:
+            images = [None] * len(texts)
 
         for image, text in zip(images, texts):
             try:
