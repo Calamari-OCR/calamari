c9b7110249b8317e4520943b7b42b045ccaf22a3
Gerber, Mike <mike.gerber@sbb.spk-berlin.de>
2019-08-06 18:57:23 +0200

    Fix tests
    With these fixes, the tests run, when running them from the test/
    directory. However, there still is the issue that they depend on each
    other by using results from a previous test training.

--- calamari_ocr/test/test_validation_train.py
+++ calamari_ocr/test/test_validation_train.py
@@ -1,13 +1,13 @@
 import unittest
 import os
 
-from .test_simple_train import Attrs, this_dir, run, glob_all
-
+from calamari_ocr.test.test_simple_train import Attrs, this_dir, run, glob_all
 
 class TestValidationTrain(unittest.TestCase):
     def test_validation_train(self):
         args = Attrs()
         args.validation = glob_all([os.path.join(this_dir, "data", "uw3_50lines", "test", "*.png")])
+        args.validation_text_files = None
         args.max_iters = 30000
 
         run(args)
@@ -15,6 +15,7 @@ class TestValidationTrain(unittest.TestCase):
     def test_validation_pretrain(self):
         args = Attrs()
         args.validation = glob_all([os.path.join(this_dir, "data", "uw3_50lines", "test", "*.png")])
+        args.validation_text_files = None
         args.max_iters = 1000
         args.early_stopping_best_model_prefix = args.early_stopping_best_model_prefix + "pretrain_"
         args.weights = os.path.join(this_dir, "test_models", "uw3_50lines_best.ckpt")
