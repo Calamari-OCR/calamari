Fixed default computation for early stopping callback based on steps_per_epoch_train

calamari_ocr/ocr/backends/tensorflow_backend/callbacks/earlystopping.py
@@ -10,13 +10,13 @@ keras = tf.keras
 
 
 class EarlyStoppingCallback(keras.callbacks.Callback):
-    def __init__(self, training_callback,  codec, data_gen, predict_func, checkpoint_params, steps_per_epoch, vis_cb, progress_bar):
+    def __init__(self, training_callback,  codec, data_gen, predict_func, checkpoint_params, steps_per_epoch_valid, steps_per_epoch_train, vis_cb, progress_bar):
         self.training_callback = training_callback
         self.codec = codec
         self.data_gen = data_gen
         self.predict_func = predict_func
         self.checkpoint_params = checkpoint_params
-        self.steps_per_epoch = steps_per_epoch
+        self.steps_per_epoch_valid = steps_per_epoch_valid
         self.vis_cb = vis_cb
         self.progress_bar = progress_bar
 
@@ -26,9 +26,9 @@ class EarlyStoppingCallback(keras.callbacks.Callback):
             # set early stopping frequency to half epoch
             # batch size only with square root:
             # esf = 0.5 * epoch_size / sqrt(batch_size) = 0.5 * iters_per_epoch * sqrt(batch_size)
-            early_stopping_frequency = int(0.5 * steps_per_epoch * checkpoint_params.batch_size ** 0.5)
+            early_stopping_frequency = int(0.5 * steps_per_epoch_train * checkpoint_params.batch_size ** 0.5)
         elif 0 < early_stopping_frequency <= 1:
-            early_stopping_frequency = int(early_stopping_frequency * steps_per_epoch)  # relative to epochs
+            early_stopping_frequency = int(early_stopping_frequency * steps_per_epoch_train)  # relative to epochs
         else:
             early_stopping_frequency = int(early_stopping_frequency)
         early_stopping_frequency = max(1, early_stopping_frequency)
@@ -36,7 +36,7 @@ class EarlyStoppingCallback(keras.callbacks.Callback):
         if checkpoint_frequency < 0:
             checkpoint_frequency = early_stopping_frequency
         elif 0 < checkpoint_frequency <= 1:
-            checkpoint_frequency = int(checkpoint_frequency * steps_per_epoch)  # relative to epochs
+            checkpoint_frequency = int(checkpoint_frequency * steps_per_epoch_train)  # relative to epochs
         else:
             checkpoint_frequency = int(checkpoint_frequency)
 
@@ -109,7 +109,7 @@ class EarlyStoppingCallback(keras.callbacks.Callback):
 
         if self.early_stopping_enabled and (iter + 1) % self.early_stopping_frequency == 0:
             print("Checking early stopping model")
-            cer = self._compute_current_cer_on_validation_set(self.steps_per_epoch)
+            cer = self._compute_current_cer_on_validation_set(self.steps_per_epoch_valid)
             accuracy = 1 - cer
 
             if accuracy > self.early_stopping_best_accuracy:

calamari_ocr/ocr/backends/tensorflow_backend/tensorflow_model.py
@@ -317,7 +317,9 @@ class TensorflowModel(ModelInterface):
         predict_func = K.function({t.op.name: t for t in [self.input_data, self.input_length, self.input_params, self.targets, self.targets_length]}, [self.cer, self.sparse_targets, self.sparse_decoded])
         steps_per_epoch = max(1, int(dataset.epoch_size() / checkpoint_params.batch_size))
         v_cb = VisCallback(training_callback, self.codec, dataset_gen, predict_func, checkpoint_params, steps_per_epoch, text_post_proc)
-        es_cb = EarlyStoppingCallback(training_callback, self.codec, val_dataset_gen, predict_func, checkpoint_params, 0 if not validation_dataset else max(1, int(np.ceil(validation_dataset.epoch_size() / checkpoint_params.batch_size))), v_cb, progress_bar)
+        es_cb = EarlyStoppingCallback(training_callback, self.codec, val_dataset_gen, predict_func, checkpoint_params,
+                                      0 if not validation_dataset else max(1, int(np.ceil(validation_dataset.epoch_size() / checkpoint_params.batch_size))),
+                                      steps_per_epoch, v_cb, progress_bar)
 
         self.model.fit(
             dataset_gen,