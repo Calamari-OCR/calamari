de8766084574985f1bf692d2b8b12de41b90d219
ocrwork <49283839+ocrwork@users.noreply.github.com>
2020-03-08 11:44:47 -0700

    tensorflow only use amount of memory needed

--- calamari_ocr/ocr/backends/tensorflow_backend/tensorflow_model.py
+++ calamari_ocr/ocr/backends/tensorflow_backend/tensorflow_model.py
@@ -2,7 +2,15 @@ import tensorflow as tf
 import numpy as np
 import json
 from typing import Generator
-
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+  except RuntimeError as e:
+    # Memory growth must be set before GPUs have been initialized
+    print(e)
 
 from calamari_ocr.ocr.backends.model_interface import ModelInterface, NetworkPredictionResult
 from calamari_ocr.ocr.callbacks import TrainingCallback
