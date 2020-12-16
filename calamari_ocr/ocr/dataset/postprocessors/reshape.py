6d52f1aac99d2e391a7881e9f52ef43477ffe6ba
C. Wick <christoph.wick@planet-ai.de>
2020-12-16 09:26:33 +0100

    Fixed prediction with different batch sizes dropping some requirements that are subsummed in tfaip (e.g. tensorflow and numpy)

--- calamari_ocr/ocr/dataset/postprocessors/reshape.py
+++ calamari_ocr/ocr/dataset/postprocessors/reshape.py
@@ -6,6 +6,10 @@ class ReshapeOutputsProcessor(DataProcessor):
         assert(inputs['img_len'].shape == (1,))
         assert(inputs['meta'].shape == (1,))
         inputs = inputs.copy()
+        outputs = outputs.copy()
         inputs['img_len'] = inputs['img_len'][0]
         inputs['meta'] = inputs['meta'][0]
+        for name in {'logits', 'softmax', 'blank_last_logits', 'blank_last_softmax'}:
+            if name in outputs:
+                outputs[name] = outputs[name][:outputs['out_len']]
         return inputs, outputs
