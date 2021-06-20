Fix int 16 scaling

calamari_ocr/utils/image.py
@@ -90,7 +90,7 @@ def to_uint8(data: np.ndarray) -> np.ndarray:
     elif data.dtype == bool:
         data = data.astype("uint8") * 255
     else:
-        raise Exception("unknown image type: {}".format(data.dtype))
+        raise Exception(f"Unknown image type: {data.dtype}")
 
     return data
 
@@ -105,14 +105,14 @@ def to_float32(data: np.ndarray) -> np.ndarray:
     elif data.dtype == np.dtype("int8"):
         data = (data.astype("int16") + 128).astype("float32") / 255
     elif data.dtype == np.dtype("uint16"):
-        data = data.astype("float32") / 256 / 255
+        data = data.astype("float32") / 65535
     elif data.dtype == np.dtype("int16"):
-        data = ((data.astype("float32") / 128) + 128) / 255
+        data = (data.astype("float32") + 32768) / 65535
     elif data.dtype in [np.dtype("f"), np.dtype("float32"), np.dtype("float64")]:
         data = data.astype("float32")
     elif data.dtype == bool:
         data = data.astype("float32")
     else:
-        raise Exception("unknown image type: {}".format(data.dtype))
+        raise Exception(f"Unknown image type: {data.dtype}")
 
     return data