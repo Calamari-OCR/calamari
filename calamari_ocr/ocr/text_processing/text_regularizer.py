6b1412b997b6830935e28a0234071352e09a08c2
Mike Gerber <mike.gerber@sbb.spk-berlin.de>
2023-10-24 11:48:24 +0200

    Move global flags to the start of regular expressions (#349)
    * Revert "Move global flags to the start of regular expressions (fix for issue #348)"
    
    This reverts commit 13b544b1d2eb29617000dbbdd38d0405226edd33.
    
    * Move global flags to the start of regular expressions
    
    Fix regex replacements for models where global flags were put at the end
    of the pattern strings. These patterns are invalid as of Python 3.11.

--- calamari_ocr/ocr/text_processing/text_regularizer.py
+++ calamari_ocr/ocr/text_processing/text_regularizer.py
@@ -72,7 +72,7 @@ def default_text_regularizer_params(params=TextProcessorParams(), groups=["simpl
 
     def replacement(old, new, regex=False):
         r = params.replacements.add()
-        r.old = "(?u)" + old[:-4] if regex and old.endswith("(?u)") else old
+        r.old = old
         r.new = new
         r.regex = regex
 
@@ -343,6 +343,14 @@ class TextRegularizer(TextProcessor):
     def __init__(self, params=default_text_regularizer_params()):
         super().__init__()
         self.params = params
+        self._fix_regex_flags()
+
+    def _fix_regex_flags(self):
+        # Fix regex replacements for older models where global flags were put at the end
+        # of the pattern string. These patterns are invalid as of Python 3.11.
+        for replacement in self.params.replacements:
+            if replacement.regex and replacement.old.endswith("(?u)"):
+                replacement.old = "(?u)" + replacement.old[:-4]
 
     def _apply_single(self, txt):
         for replacement in self.params.replacements:
