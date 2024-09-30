2e6eeb6c7810136f4dbd26ff7984ec1ad7fae8c9
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2024-09-30 22:44:59 +0200

    test_command_line: fix (subprocess.run w/ assertion instead of check_call, separate output via --output_dir instead of --pred_extension)

--- calamari_ocr/test/test_command_line.py
+++ calamari_ocr/test/test_command_line.py
@@ -2,17 +2,25 @@ import os
 import tempfile
 import unittest
 from glob import glob
-from subprocess import check_call
+from subprocess import check_call, run
 import uuid
 
 this_dir = os.path.dirname(os.path.realpath(__file__))
 
 
+def assert_run(args):
+    # cannot use check_call, because it blocks if the stdout FIFO is full
+    # (i.e. there is sufficient data on the OS pipe buffer) and it is not read from
+    # also, we want to see what failed in case of error
+    #check_call(args)
+    ret = run(args, encoding='utf-8', capture_output=True)
+    assert ret.returncode == 0
+
 class TestCommandList(unittest.TestCase):
     def test_resume_training(self):
         with tempfile.TemporaryDirectory() as d:
             # Test training
-            check_call(
+            assert_run(
                 [
                     "calamari-train",
                     "--train.images",
@@ -30,68 +38,61 @@ class TestCommandList(unittest.TestCase):
                     "--network=cnn=4:3x3,pool=4x4,lstm=10,dropout=0.5",
                 ]
             )
-            check_call(
+            assert_run(
                 ["calamari-resume-training", os.path.join(d, "checkpoint", "checkpoint_0001", "trainer_params.json")]
             )
 
     def test_command_line(self):
-        pred_extension = "." + str(uuid.uuid4()) + ".pred.txt"
-        try:
-            with tempfile.TemporaryDirectory() as d:
-                # Test training
-                check_call(
-                    [
-                        "calamari-train",
-                        "--train.images",
-                        os.path.join(this_dir, "data", "uw3_50lines", "train", "*.bin.png"),
-                        "--trainer.epochs",
-                        "1",
-                        "--trainer.samples_per_epoch",
-                        "16",
-                        "--trainer.output_dir",
-                        d,
-                        "--n_augmentations",
-                        "2",
-                        "--trainer.gen",
-                        "SplitTrain",
-                        "--network=cnn=4:3x3,pool=4x4,lstm=10,dropout=0.5",
-                    ]
-                )
-                # Test single model prediction
-                check_call(
-                    [
-                        "calamari-predict",
-                        "--data.images",
-                        os.path.join(this_dir, "data", "uw3_50lines", "test", "*.bin.png"),
-                        "--data.pred_extension",
-                        pred_extension,
-                        "--checkpoint",
-                        os.path.join(d, "best.ckpt.json"),
-                    ]
-                )
-                # Test voting
-                check_call(
-                    [
-                        "calamari-predict",
-                        "--data.images",
-                        os.path.join(this_dir, "data", "uw3_50lines", "test", "*.bin.png"),
-                        "--data.pred_extension",
-                        pred_extension,
-                        "--checkpoint",
-                        os.path.join(d, "best.ckpt.json"),
-                        os.path.join(d, "best.ckpt.json"),
-                    ]
-                )
-                # Test evaluation
-                check_call(
-                    [
-                        "calamari-eval",
-                        "--gt.texts",
-                        os.path.join(this_dir, "data", "uw3_50lines", "test", "*.gt.txt"),
-                        "--gt.pred_extension",
-                        pred_extension,
-                    ]
-                )
-        finally:
-            for file in glob(os.path.join(this_dir, "data", "uw3_50lines", "test", "*" + pred_extension)):
-                os.remove(file)
+        with tempfile.TemporaryDirectory() as d:
+            # Test training
+            assert_run(
+                [
+                    "calamari-train",
+                    "--train.images",
+                    os.path.join(this_dir, "data", "uw3_50lines", "train", "*.bin.png"),
+                    "--trainer.epochs",
+                    "1",
+                    "--trainer.samples_per_epoch",
+                    "16",
+                    "--trainer.output_dir",
+                    d,
+                    "--n_augmentations",
+                    "2",
+                    "--trainer.gen",
+                    "SplitTrain",
+                    "--network=cnn=4:3x3,pool=4x4,lstm=10,dropout=0.5",
+                ]
+            )
+            # Test single model prediction
+            assert_run(
+                [
+                    "calamari-predict",
+                    "--data.images",
+                    os.path.join(this_dir, "data", "uw3_50lines", "test", "*.bin.png"),
+                    "--output_dir",
+                    d,
+                    "--checkpoint",
+                    os.path.join(d, "best.ckpt.json"),
+                ]
+            )
+            # Test voting
+            assert_run(
+                [
+                    "calamari-predict",
+                    "--data.images",
+                    os.path.join(this_dir, "data", "uw3_50lines", "test", "*.bin.png"),
+                    "--output_dir",
+                    d,
+                    "--checkpoint",
+                    os.path.join(d, "best.ckpt.json"),
+                    os.path.join(d, "best.ckpt.json"),
+                ]
+            )
+            # Test evaluation
+            assert_run(
+                [
+                    "calamari-eval",
+                    "--gt.texts",
+                    os.path.join(this_dir, "data", "uw3_50lines", "test", "*.gt.txt"),
+                ]
+            )
