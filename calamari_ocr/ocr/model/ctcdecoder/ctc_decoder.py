fix word/glyph ID's, bound boxes and add output precision option (#372)
* Fix for global char positions, added extended positions that try to cover up the space to the neighboring characters

* Relative word and glyph id's. Adds option to choose between lines/words/glyphs instead of lines/glyphs

* Clips coordinates to [0, un-padded length] in FinalPreparation processor

* added support for otf fonts

* fix test/lint

---------

Co-authored-by: @andbue

calamari_ocr/ocr/model/ctcdecoder/ctc_decoder.py
@@ -116,7 +116,7 @@ class CTCDecoder(ABC):
             p = probabilities[start:end]
             p = np.max(p, axis=0)
 
-            pos = PredictionPosition(local_start=start, local_end=end - 1)
+            pos = PredictionPosition(local_start=start, local_end=end)
             pred.positions.append(pos)
 
             for label in reversed(sorted(range(len(p)), key=lambda v: p[v])):